1a9ab832c6bf4d11b95699e8fcbacfdb55e972d2
Sean Geary <sgeary@flexera.com>
2021-03-23 20:20:03 -0400

    Improve data collection for reporting

--- report_data.py
+++ report_data.py
@@ -17,93 +17,129 @@ logger = logging.getLogger(__name__)
 
 #-------------------------------------------------------------------#
 def gather_data_for_report(baseURL, projectID, authToken, reportName, reportOptions):
-
+ 
     projectInformation = CodeInsight_RESTAPIs.project.get_project_information.get_project_information_summary(baseURL, projectID, authToken)
     projectName = projectInformation["name"]
 
-    claimableFilePaths = [] # List to hold the file paths that contain only the evidence to be claimed
-    unclaimableFilePaths = [] # List to hold the file paths that has other evidience
-    noEvidenceFilePaths = [] # List to hold files without any evidence at all
+    fileEvidence = {} # Dict to hold evidience information for each file (claimable or not)
 
     # Parse report options
     takeAction = reportOptions["takeAction"] 
-    evidence = reportOptions["evidence"]  
+    stringsToClaim = reportOptions["stringsToClaim"]  
     inventoryItem = reportOptions["inventoryItem"] 
-
+    isSearchtermClaimable = reportOptions["isSearchtermClaimable"] 
+   
     # Get the evidence gathered
     projectEvidence = CodeInsight_RESTAPIs.project.get_project_evidence.get_project_evidence(baseURL, projectID, authToken)
-    for fileEvidence in projectEvidence["data"]:
-        nonClaimedEvidenceFiles = 0
-        fileName = fileEvidence["fileName"]
-        filePath = fileEvidence["filePath"]
-        remote = fileEvidence["remote"]
-        scannedFileId = fileEvidence["scannedFileId"]
-        copyRightMatches = fileEvidence["copyRightMatches"]
-        emailUrlMatches = fileEvidence["emailUrlMatches"]
-        licenseMatches =  fileEvidence["licenseMatches"]
-        searchTextMatches =  fileEvidence["searchTextMatches"]
-        exactFileMatches =  fileEvidence["exactFileMatches"]
-        sourceMatches =  fileEvidence["sourceMatches"]
+    for evidence in projectEvidence["data"]:
+
+        fileName = evidence["fileName"]
+        filePath = evidence["filePath"]
+        remote = evidence["remote"]
+        scannedFileId = evidence["scannedFileId"]
+        copyrightEvidienceFound = evidence["copyRightMatches"]
+        emailUrlEvidenceFound = evidence["emailUrlMatches"]
+        licenseEvidenceFound =  evidence["licenseMatches"]
+        searchTextMatchEvidenceFound =  evidence["searchTextMatches"]
+        exactFileMatchEvidenceFound =  evidence["exactFileMatches"]
+        sourceMatchEvidenceFound =  evidence["sourceMatches"]
+
+        fileEvidence[filePath] = {} # Dict to hold the data for each file
+        fileEvidence[filePath]["filelink"] =  baseURL + "/codeinsight/FNCI#myprojectdetails/?id=" + str(projectID) + "&tab=workbench&view=file&fileid=" + str(scannedFileId) + "&remote=" + str(remote)
 
         # See if there was any evidence at all and if so then look further
-        if len(copyRightMatches) or len(emailUrlMatches) or len(licenseMatches):
-            hasEvidence = True
+        if len(copyrightEvidienceFound) or len(emailUrlEvidenceFound) or len(licenseEvidenceFound):
+            fileEvidence[filePath]["hasEvidence"] = True
+            fileEvidence[filePath]["claimableEvidence"] = {} # Dict to hold the data for each file
+            fileEvidence[filePath]["nonclaimableEvidence"] = {} # Dict to hold the data for each file
         
             # For each file see if there is something other than what we are looking for
             # if so this file would need review since there is more too it
-            for copyright in copyRightMatches:
-                if evidence.lower() not in copyright.lower():
-                    nonClaimedEvidenceFiles +=1
-                    break
+            for copyright in copyrightEvidienceFound:
+                if stringsToClaim.lower() in copyright.lower():
+                    fileEvidence[filePath]["claimableEvidence"]["copyright"] = True
+                else:
+                    fileEvidence[filePath]["nonclaimableEvidence"]["copyright"] = True
 
-            for emailURL in emailUrlMatches:
-                if evidence.lower() not in emailURL.lower():
-                    nonClaimedEvidenceFiles +=1
-                    break
-
-            for license in licenseMatches:
-                if evidence.lower() not in license.lower():
-                    nonClaimedEvidenceFiles +=1
-                    break      
-        
-        else:
-            hasEvidence = False
+            for emailURL in emailUrlEvidenceFound:
+                if stringsToClaim.lower() in emailURL.lower():
+                    fileEvidence[filePath]["claimableEvidence"]["emailURL"] = True
+                else:
+                    fileEvidence[filePath]["nonclaimableEvidence"]["emailURL"] = True
       
-        # Break down the scanned files based on what was found
-        if hasEvidence:
-            # Is it evidence that we wnat to claim?
-            if nonClaimedEvidenceFiles == 0:
-                claimableFilePaths.append(filePath)
-            else:
-                unclaimableFilePaths.append(filePath)
-        else:
-            noEvidenceFilePaths.append(filePath)  
+            for license in licenseEvidenceFound:
+                if stringsToClaim.lower() in license.lower():
+                    fileEvidence[filePath]["claimableEvidence"]["license"] = True
+                else:
+                    fileEvidence[filePath]["nonclaimableEvidence"]["license"] = True
 
+            if searchTextMatchEvidenceFound:
+                fileEvidence[filePath]["searchTerm"] = True
+            else:
+                fileEvidence[filePath]["searchTerm"] = False 
+
+            # With an exact match or a source match we want to make sure the file
+            # cannot be claimed automatically
+            if exactFileMatchEvidenceFound:
+                fileEvidence[filePath]["nonclaimableEvidence"]["exactFile"] = True
+
+            if sourceMatchEvidenceFound:
+                fileEvidence[filePath]["nonclaimableEvidence"]["sourceMatch"] = True
+                         
+        else:
+            fileEvidence[filePath]["hasEvidence"] = False
+
+
+    # Now create seperate dictionaries to hold the data for each catagory
+    claimableFiles = {}
+    nonclaimableFiles = {}
+
+    # Only worry about the files that contain the evidience to be claimed
+    for filePath in fileEvidence:
+        if fileEvidence[filePath]["hasEvidence"]:
+
+            # Does the file contain both claimable and nonclaimable evidence?
+            if fileEvidence[filePath]["claimableEvidence"] and fileEvidence[filePath]["nonclaimableEvidence"]:
+                if fileEvidence[filePath]["searchTerm"]: 
+                    fileEvidence[filePath]["nonclaimableEvidence"]["searchTerm"] = True
+                nonclaimableFiles[filePath] = fileEvidence[filePath]
+            elif fileEvidence[filePath]["claimableEvidence"]:
+
+                # For a serach team check to see if the user wants these to be considered or not
+                # prior to claiming.  If True then a search term match will not prevent the file
+                # from being claimed.  Files need to be compared to see if they can be claimed 
+                # already and then check against this criteria
+
+                if isSearchtermClaimable.lower() == "true":
+                    if fileEvidence[filePath]["searchTerm"]: 
+                        fileEvidence[filePath]["claimableEvidence"]["searchTerm"] = True
+                    claimableFiles[filePath] = fileEvidence[filePath]
+                else:
+                    if fileEvidence[filePath]["searchTerm"]:
+                        fileEvidence[filePath]["nonclaimableEvidence"]["searchTerm"] = True
+                        nonclaimableFiles[filePath] = fileEvidence[filePath]
+                    else:
+                        claimableFiles[filePath] = fileEvidence[filePath]
+                    
 
     if takeAction.lower() == "true":
         # First see if there is already an inventory item to assign the files to
         takeAction = True
-
-
-        # Add the files to it
-        for file in claimableFilePaths:
-            logger.debug("Adding %s to %s" %(file, inventoryItem))
+        for filePath in claimableFiles:
+            print("Claiming %s" %filePath)
     else:
         takeAction = False
 
-    
     
     # Build up the data to return for the report generation
     reportData = {}
     reportData["reportName"] = reportName
     reportData["projectName"] = projectName
     reportData["takeAction"] = takeAction
-    reportData["evidence"] = evidence
+    reportData["stringsToClaim"] = stringsToClaim
     reportData["inventoryItem"] = inventoryItem
-
-    reportData["claimableFilePaths"] = claimableFilePaths
-    reportData["unclaimableFilePaths"] = unclaimableFilePaths
-    reportData["noEvidenceFilePaths"] = noEvidenceFilePaths
+    reportData["claimableFiles"] = claimableFiles
+    reportData["nonclaimableFiles"] = nonclaimableFiles
     
     
     return reportData
